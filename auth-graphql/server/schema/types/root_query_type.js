9c600d3a08794133ae23a70c1617dcd530b44448
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-05-03 12:29:38 +0530

    checking auth status

--- auth-graphql/server/schema/types/root_query_type.js
+++ auth-graphql/server/schema/types/root_query_type.js
@@ -1,10 +1,18 @@
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLID } = graphql;
 
+const UserType = require("./user_type");
+
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    dummyField: { type: GraphQLID },
+    user: {
+      type: UserType,
+      resolve(_parentValue, _args, req) {
+        // if a user is currently signed in, return that user. otherwise return undefined
+        return req.user;
+      },
+    },
   },
 });
 
