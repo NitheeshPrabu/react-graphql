1fb1cbfd823b3f190bf15b4408aaaeba7fb7ab8a
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-05-03 13:07:50 +0530

    logout client mutation

--- auth-graphql/client/index.js
+++ auth-graphql/client/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { Router, hashHistory, Route, IndexRoute } from "react-router";
 
-const Root = () => {
-  return (
-    <div>
-      Auth Starter
-    </div>
-  );
-};
+import App from "./components/App";
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+const networkInterface = createNetworkInterface({
+  uri: "/graphql",
+  opts: {
+    // credentials tells us that the request is made to the browser which is in the same origin
+    // in this case, used to tell that it is safe to send cookies along with the queries sent to the backend
+    credentials: "same-origin",
+  },
+});
+
+const client = new ApolloClient({
+  dataIdFromObject: (o) => o.id,
+  networkInterface,
+});
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App} />
+    </Router>
+  </ApolloProvider>
+);
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
