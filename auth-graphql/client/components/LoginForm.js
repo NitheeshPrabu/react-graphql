15f9d5c9df49f9421b2af9ad709595693ee101f5
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2020-05-03 15:29:22 +0530

    requireAuth hoc

--- auth-graphql/client/components/LoginForm.js
+++ auth-graphql/client/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
+import { hashHistory } from "react-router";
 
 import AuthForm from "./AuthForm";
 import mutation from "../mutations/Login";
@@ -9,6 +10,12 @@ class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push("/dashboard");
+    }
   }
 
   onSubmit({ email, password }) {
